75d283c2241949552c99db2efd8ab4cb7504b55e
Andrea Diaz <aidiaz5@uc.cl>
2022-09-26 21:56:01 -0300

    render pokemons on screen

--- main.js
+++ main.js
@@ -1,30 +1,91 @@
-// const pokemonContainer = document.querySelector('.pokemon-container');
+const pokemonContainer = document.querySelector(".pokemon-container");
 
-// Petición al Api
-// function fetchPokemon(id) {
-//     fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-//         .then(res => res.json())
-//         .then(data => console.log(data))
-// }
-
-// fetchPokemon(1);
-
-
-// MÉTODO QUE VIMOS EN CLASES:
-
-async function getPokemones(id) {
+//Fetch al API que trae un pokemon
+async function getPokemonData(id) {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`); //el fetch es asíncrono porque en el fondo de su corazón es una promesa.
-    const data = await response.json(); //el json también es asíncrono.
-    console.log('3', data.name);
+    const pokemonData = await response.json(); //el json también es asíncrono.
+    // const pokemon = data;
+    // console.log(pokemonData);
+    createCard(pokemonData);
+
+    return pokemonData
 }
 
-async function main() {
-    console.log('1');
-    console.log('2');
-    await getPokemones(1)
-    console.log('4');
-}
-main();
+//For para traer varios pokemones // cambiarlo por un for each
+// async function getPokemons(number) {
+//     for (let i = 1; i <= number; i++) {
+//        await getPokemon(i);
+//     }
+// } //como que haciendo la lectura dado lo que dice la función pienso que debería traer 3 pokemones. Revisar. 
 
+// i++ = sumar 1
+// Preguntas: 
+// (linea 17) Por qué se usa <= y no solo =
+
+
+
+//NUEVA FUNCIÓN PARA TRANSFORMARLO EN ARRAY DE PASO
+const pokemonsToRender = [];
+async function getPokemons(number) {
+    for (let i = 1; i <= number; i++) {
+        const pokemonData = await getPokemonData(i);
+        if (i === 1) console.log(JSON.stringify(pokemonData));
+        pokemonsToRender.push(pokemonData);
+    }
+    console.log('variable para crear el array', pokemonsToRender);
+}
+
+
+function createCard(pokemon) {
+    // console.log(pokemon);
+
+    const card = document.createElement('div');
+    card.classList.add('pokemon-block');
+
+    const imageContainer = document.createElement('div');
+    imageContainer.classList.add('img-container');
+
+    const image = document.createElement('img');
+    image.src = pokemon.sprites.front_default
+
+    imageContainer.appendChild(image);
+
+    const number = document.createElement('p');
+    number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`;
+
+    const name = document.createElement('p');
+    name.classList.add('name');
+    name.textContent = pokemon.name
+
+    card.appendChild(imageContainer);
+    card.appendChild(number);
+    card.appendChild(name);
+
+    pokemonContainer.appendChild(card);
+}
+
+getPokemons(151);
+
+
+//FILTRADO DE POKEMONES
+//ARRAY METHOD: FILTER
+
+function getPokemonsByType(pokemon, type) {
+    const {types} = pokemon;
+
+    if (pokemon.types.type.name = type) {
+        createCard(pokemon)
+    }
+}
+
+
+//Función main
+// async function main() {
+//     console.log('1');
+//     console.log('2');
+//     await getPokemon(1)
+//     console.log('4');
+// }
+// main();
 
 
